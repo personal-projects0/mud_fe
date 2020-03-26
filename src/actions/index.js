b81c05a7741bb2d42815d8db3dc795e4c4f99d20
schang1146 <sammychang2185@gmail.com>
2020-03-26 11:58:19 -0700

    added actions and reducers index.js files

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+/*
+add additional actions with:
+export const ${action_name} = '${action_name}';
+*/
+
+/*
+add additional action functions with:
+export const ${func_name} = (${args}) => dispatch => {};
+*/
